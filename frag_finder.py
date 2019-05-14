14867f278b99cae795c22692f644ea527f1cabaa
Jared <user@ron-apple.local>
2019-05-14 14:32:57 +1000

    update

--- frag_finder.py
+++ frag_finder.py
@@ -47,21 +47,21 @@ def fragments(prot_seq, obs_masses, dataframe, tolerance):
                     if i == len(prot_seq):
                         find = [prot_seq[start] + str(start + 1),
                                 str(i),
-                                num, 
+                                num,
                                 round(mass.calculate_mass(prot_seq[start:i], average = True), 1),
                                 round(num - round(mass.calculate_mass(prot_seq[start:i], average = True), 1), 1)]
                         single_cut.append(find)
                     else:
                         find = [prot_seq[start] + str(start + 1),
                                 str(i),
-                                num, 
+                                num,
                                 round(mass.calculate_mass(prot_seq[start:i], average = True), 1),
                                 round(num - round(mass.calculate_mass(prot_seq[start:i], average = True), 1), 1)]
                         double_cut.append(find)
         s += 1
         e += 1
         start += 1
-    
+
     df1 = pandas.DataFrame(single_cut, columns = ['Cutsite (Nterm)', 'Cterm', 'M(obs)', 'M(calc)', 'deltaM'])
     df1.sort_values('M(obs)', inplace=True)
     df2 = pandas.DataFrame(double_cut, columns = ['Cutsite (Nterm)', 'Cutsite (Cterm)', 'M(obs)', 'M(calc)', 'deltaM'])
@@ -91,14 +91,14 @@ def fragments_multi(prot_seq, obs_mass, dataframe, tolerance):
                 if i == len(prot_seq):
                     find = [prot_seq[start] + str(start + 1),
                             str(i),
-                            obs_mass, 
+                            obs_mass,
                             round(mass.calculate_mass(prot_seq[start:i], average = True), 1),
                             round(obs_mass - round(mass.calculate_mass(prot_seq[start:i], average = True), 1), 1)]
                     single_cut.append(find)
                 else:
                     find = [prot_seq[start] + str(start + 1),
                             str(i),
-                            obs_mass, 
+                            obs_mass,
                             round(mass.calculate_mass(prot_seq[start:i], average = True), 1),
                             round(obs_mass - round(mass.calculate_mass(prot_seq[start:i], average = True), 1), 1)]
                     double_cut.append(find)
@@ -117,9 +117,9 @@ def fragments_multi(prot_seq, obs_mass, dataframe, tolerance):
     #df1_i['I'] = percent_i
     #df1_i.rename(columns={'I':'% Intensity'}, inplace=True)
 
-    if len(single_cut) != 0:
-        #print(df1_i.to_string(index=False))
-        return(single_cut)
+    #if len(single_cut) != 0:
+    #    return(single_cut)
+    return [single_cut, double_cut]
 
 def main():
     input = argparse.ArgumentParser()
@@ -143,22 +143,24 @@ def main():
     if __name__ == '__main__':
         with multiprocessing.Pool(processes = args.number_of_cores) as pool:
             results = pool.starmap(fragments_multi, multi)
-            results = [i for i in results if i is not None]
+            #results = [i for i in results if i is not None]
+            df1 = pandas.DataFrame(results)
+            print(df1)
 
-            df1 = pandas.DataFrame(results, columns = ['one'])
-            df1[['Cutsite (Nterm)', 'Cterm', 'M(obs)', 'M(calc)', 'deltaM']] = pandas.DataFrame(df1.one.values.tolist(), index= None)
-            df1.drop(columns="one", inplace = True)
-            df1.sort_values('M(obs)', inplace = True)
+            #df1 = pandas.DataFrame(results, columns = ['one'])
+            #df1[['Cutsite (Nterm)', 'Cterm', 'M(obs)', 'M(calc)', 'deltaM']] = pandas.DataFrame(df1.one.values.tolist(), index= None)
+            #df1.drop(columns="one", inplace = True)
+            #df1.sort_values('M(obs)', inplace = True)
             #df2 = pandas.DataFrame(double_cut, columns = ['Cutsite (Nterm)', 'Cutsite (Cterm)', 'M(obs)', 'M(calc)', 'deltaM'])
             #df2.sort_values('M(obs)', inplace=True)
-            df_i = dataframe[['M(obs)', 'I']]
-            df1_i = pandas.merge(df1, df_i, on= 'M(obs)', how='right')
-            df1_i.dropna(how = 'any', inplace = True)
-            percent_i = [round(((num / max(df1_i['I'])) * 100), 2) for num in df1_i['I']]
-            df1_i['I'] = percent_i
-            df1_i.rename(columns={'I':'% Intensity'}, inplace=True)
+            #df_i = dataframe[['M(obs)', 'I']]
+            #df1_i = pandas.merge(df1, df_i, on= 'M(obs)', how='right')
+            #df1_i.dropna(how = 'any', inplace = True)
+            #percent_i = [round(((num / max(df1_i['I'])) * 100), 2) for num in df1_i['I']]
+            #df1_i['I'] = percent_i
+            #df1_i.rename(columns={'I':'% Intensity'}, inplace=True)
 
-        print(df1_i)  
+        #print(df1_i)
 
 if __name__ == "__main__":
     main()
