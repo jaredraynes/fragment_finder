6c8ce392125f00eb9331063ad67c32d0da2db9dc
Raynes, Jared (A&F, Werribee SnydRd) <Jared.Raynes@csiro.au>
2019-05-15 11:44:19 +1000

    full working program

--- frag_finder.py
+++ frag_finder.py
@@ -21,15 +21,12 @@ def import_dataframe(file_location):
     return(dataframe)
 
 def mass_cal(peptide_seq):
+
     return(round(mass.calculate_mass(peptide_seq, average = True), 1))
 
 def import_obs_masses(dataframe):
+
     return(list(dataframe['M(obs)']))
-
-def mass_diff(prot_mass, obs_masses):
-
-    mass_diffs = [prot_mass - masses for masses in obs_masses]
-    return(mass_diffs)
 
 def fragments(prot_seq, obs_masses, dataframe, tolerance):
 
@@ -78,8 +75,7 @@ def fragments(prot_seq, obs_masses, dataframe, tolerance):
 
 def fragments_multi(prot_seq, obs_mass, dataframe, tolerance):
 
-    single_cut = []
-    double_cut = []
+    found = []
     start = 0
     s = int(obs_mass)//107
     e = int(obs_mass)//95
@@ -89,37 +85,26 @@ def fragments_multi(prot_seq, obs_mass, dataframe, tolerance):
                 break
             if math.isclose(round(mass.calculate_mass(prot_seq[start:i], average = True), 1), obs_mass, abs_tol = tolerance):
                 if i == len(prot_seq):
-                    find = [prot_seq[start] + str(start + 1),
+                    find = ['single', 
+                            prot_seq[start] + str(start + 1),
                             str(i),
                             obs_mass,
                             round(mass.calculate_mass(prot_seq[start:i], average = True), 1),
                             round(obs_mass - round(mass.calculate_mass(prot_seq[start:i], average = True), 1), 1)]
-                    single_cut.append(find)
+                    found.append(find)
                 else:
-                    find = [prot_seq[start] + str(start + 1),
+                    find = ['double', 
+                            prot_seq[start] + str(start + 1),
                             str(i),
                             obs_mass,
                             round(mass.calculate_mass(prot_seq[start:i], average = True), 1),
                             round(obs_mass - round(mass.calculate_mass(prot_seq[start:i], average = True), 1), 1)]
-                    double_cut.append(find)
+                    found.append(find)
         s += 1
         e += 1
         start += 1
 
-    #df1 = pandas.DataFrame(single_cut, columns = ['Cutsite (Nterm)', 'Cterm', 'M(obs)', 'M(calc)', 'deltaM'])
-    #df1.sort_values('M(obs)', inplace=True)
-    #df2 = pandas.DataFrame(double_cut, columns = ['Cutsite (Nterm)', 'Cutsite (Cterm)', 'M(obs)', 'M(calc)', 'deltaM'])
-    #df2.sort_values('M(obs)', inplace=True)
-    #df_i = dataframe[['M(obs)', 'I']]
-    #df1_i = pandas.merge(df1, df_i, on= 'M(obs)', how='right')
-    #df1_i.dropna(how = 'any', inplace = True)
-    #percent_i = [round(((num / max(df1_i['I'])) * 100), 2) for num in df1_i['I']]
-    #df1_i['I'] = percent_i
-    #df1_i.rename(columns={'I':'% Intensity'}, inplace=True)
-
-    #if len(single_cut) != 0:
-    #    return(single_cut)
-    return [single_cut, double_cut]
+    return(found)
 
 def main():
     input = argparse.ArgumentParser()
@@ -128,39 +113,26 @@ def main():
     input.add_argument("-t", "--mass_tolerance", help = 'kDa tolerance for observed masses vs calculated masses', type = float, default = 0.5)
     input.add_argument("-s", "--save_output_file", help = 'Input the exact location where you would like your file saved, e.g. "~/XXX/XXXX/masses.xlsx"', type = str)
     input.add_argument("-c", "--number_of_cores", help = 'Input the number of processing cores your computer has. The default is 2', type = int, default = 2)
+    
     args = input.parse_args()
     dataframe = import_dataframe(args.obs_mass_input_file)
     observed_masses = import_obs_masses(dataframe)
-
     multi = [(args.protein_sequence, mass, dataframe, args.mass_tolerance) for mass in observed_masses]
-    #fragments(args.protein_sequence, observed_masses, dataframe, args.mass_tolerance)
-
-    #if __name__ == '__main__':
-    #    with multiprocessing.Pool(processes = args.number_of_cores) as pool:
-    #        results = pool.starmap(fragments_multi, multi)
-    #    print(results)
 
     if __name__ == '__main__':
         with multiprocessing.Pool(processes = args.number_of_cores) as pool:
             results = pool.starmap(fragments_multi, multi)
-            #results = [i for i in results if i is not None]
-            df1 = pandas.DataFrame(results)
-            print(df1)
+            combined = [index for line in results for index in line]
+            df1 = pandas.DataFrame(combined , columns = ['# Cuts', 'Cutsite (Nterm)', 'Cterm', 'M(obs)', 'M(calc)', 'deltaM'])
+            df_i = dataframe[['M(obs)', 'I']]
+            df1_i = pandas.merge(df1, df_i, on= 'M(obs)', how='right')
+            df1_i.dropna(how = 'any', inplace = True)
+            percent_i = [round(((num / max(df1_i['I'])) * 100), 2) for num in df1_i['I']]
+            df1_i['I'] = percent_i
+            df1_i.rename(columns={'I':'% Intensity'}, inplace=True)
+            df1_i.sort_values(['# Cuts', 'M(obs)'], ascending = [False, True], inplace=True)
 
-            #df1 = pandas.DataFrame(results, columns = ['one'])
-            #df1[['Cutsite (Nterm)', 'Cterm', 'M(obs)', 'M(calc)', 'deltaM']] = pandas.DataFrame(df1.one.values.tolist(), index= None)
-            #df1.drop(columns="one", inplace = True)
-            #df1.sort_values('M(obs)', inplace = True)
-            #df2 = pandas.DataFrame(double_cut, columns = ['Cutsite (Nterm)', 'Cutsite (Cterm)', 'M(obs)', 'M(calc)', 'deltaM'])
-            #df2.sort_values('M(obs)', inplace=True)
-            #df_i = dataframe[['M(obs)', 'I']]
-            #df1_i = pandas.merge(df1, df_i, on= 'M(obs)', how='right')
-            #df1_i.dropna(how = 'any', inplace = True)
-            #percent_i = [round(((num / max(df1_i['I'])) * 100), 2) for num in df1_i['I']]
-            #df1_i['I'] = percent_i
-            #df1_i.rename(columns={'I':'% Intensity'}, inplace=True)
-
-        #print(df1_i)
+        print(df1_i.to_string(index=False))
 
 if __name__ == "__main__":
     main()
